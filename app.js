4040b70ffab245d571b0e0f9f9e435a1879eae39
saikatberabca <saikat9382@gmail.com>
2025-01-15 11:02:34 +0530

    Add EJS layout support and enhance sign-in page design

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const userRoutes = require("./routes/user.js");
+const path = require("path");
+const ejsMate = require("ejs-mate");
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate);
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/user", userRoutes);
 
